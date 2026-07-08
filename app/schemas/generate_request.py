d141d5a115f2bb80a7a67852bb2ffeb47bb629f7
Youssef Oussous <oussousyoussef023@gmail.com>
2026-07-08 18:27:06 +0100

    seperate validation for each task type

--- app/schemas/generate_request.py
+++ app/schemas/generate_request.py
@@ -1,45 +1,100 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
 from typing import Optional
+
+from app.enums.inference_steps import InferenceSteps
 from app.enums.task_type import TaskType
-from pydantic import model_validator
+from app.enums.resulations import Resolution
 
 
 class GenerateRequest(BaseModel):
 
     task_type: TaskType
-    ref_imgs: Optional[list[str]] = None
     prompt: str
-    
-    duration: Optional[int] = 5
-    resolution: Optional[str] = "720P"
-    seed: Optional[int] = 42
-    num_inference_steps: Optional[int] = 25
-    guidance_scale: Optional[float] = 5.0
-    guidance_scale_img: Optional[float] = 3.0
-    negative_prompt: Optional[str] = ""
-    offload: Optional[bool] = False
-    low_vram: Optional[bool] = False
+
+    ref_imgs: Optional[list[str]] = None
     input_video: Optional[str] = None
     input_image: Optional[str] = None
     input_audio: Optional[str] = None
 
+    duration: int = 5
+    resolution: Resolution = Resolution.P720
+    seed: int = 42
+    num_inference_steps: InferenceSteps = InferenceSteps.HighQuality
+    guidance_scale: float = 5.0
+    guidance_scale_img: float = 3.0
+    negative_prompt: str = ""
+    offload: bool = False
+    low_vram: bool = False
+
+
+
     @model_validator(mode="after")
-    def validate_task_type(self):
+
+    def validate_request(self):
 
         if self.task_type == TaskType.REFERENCE_TO_VIDEO:
-            if not self.ref_imgs:
-                raise ValueError("ref_imgs is required for REFERENCE_TO_VIDEO task type")
-            
+
+            self.validate_reference_to_video()
+
         elif self.task_type == TaskType.SINGLE_SHOT_EXTENSION:
-            if not self.input_video:
-                raise ValueError("input_video is required for SINGLE_SHOT_EXTENSION task type")
-            
+
+            self.validate_single_shot_extension()
+
         elif self.task_type == TaskType.SHOT_SWITCHING_EXTENSION:
-            if not self.input_video:
-                raise ValueError("input_video is required for SHOT_SWITCHING_EXTENSION task type")
-            
+
+            self.validate_shot_switching_extension()
+
         elif self.task_type == TaskType.TALKING_AVATAR:
-            if not self.input_audio or not self.input_image:
-                raise ValueError("input_audio and input_image are required for TALKING_AVATAR task type")
-            
+
+            self.validate_talking_avatar()
+
         return self
+    
+    def validate_reference_to_video(self) :
+        if not self.ref_imgs:
+            raise ValueError("ref_imgs is required for REFERENCE_TO_VIDEO")
+
+        if len(self.ref_imgs) > 4:
+            raise ValueError("A maximum of 4 reference images is allowed")
+
+        if self.duration < 1 or self.duration > 5:
+            raise ValueError("duration must be between 1 and 5 for REFERENCE_TO_VIDEO")
+
+    def validate_single_shot_extension(self):
+
+        if not self.input_video:
+            raise ValueError("input_video is required for SINGLE_SHOT_EXTENSION")
+
+        if self.duration < 5 or self.duration > 30:
+            raise ValueError("duration must be between 5 and 30 for SINGLE_SHOT_EXTENSION")
+
+    def validate_shot_switching_extension(self):
+        if not self.input_video:
+            raise ValueError("input_video is required for SHOT_SWITCHING_EXTENSION")
+
+        if self.duration < 1 or self.duration > 5:
+            raise ValueError("duration must be between 1 and 5 for SHOT_SWITCHING_EXTENSION")
+
+    def validate_talking_avatar(self):
+
+        if not self.input_image:
+            raise ValueError("input_image is required for TALKING_AVATAR")
+
+        if not self.input_audio:
+            raise ValueError("input_audio is required for TALKING_AVATAR")
+
+        if self.duration < 1 or self.duration > 200:
+            raise ValueError("duration must be between 1 and 200 for TALKING_AVATAR")
+
+
+
+
+
+
+
+
+
+
+
+
+
